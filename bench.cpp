6898719ac8f1dfd61c592b18df8e59736e6c766d
Elizaveta Mironovich <elizavetamironvich@yandex.ru>
2019-10-28 23:19:13 +0300

    Change bench to use Solver class

--- bench.cpp
+++ bench.cpp
@@ -1,10 +1,9 @@
 #include "datagen.h"
-#include "pnpf_solvers.h"
+#include "solvers.h"
 #include <Eigen/Dense>
 #include <benchmark/benchmark.h>
 
 template <class Type> void BM_P35P(benchmark::State &state) {
-  Type eps = 1e-4;
   // allocate for generated data
   Matrix<Type, 3, 4> points_3d;
   Matrix<Type, 2, 4> points_2d;
@@ -19,10 +18,10 @@ template <class Type> void BM_P35P(benchmark::State &state) {
   Matrix<Type, 3, 1> T_sol_data[10];
 
   generateData(points_3d, points_2d, f_gen, R_gen, C_gen);
-
+  P35PSolver<Type> p35pSolver;
   for (auto _ : state) {
-    p35p_solver(points_3d, points_2d, eps, &solution_num, f_sol_data,
-                R_sol_data, T_sol_data);
+    p35pSolver.solve(points_3d, points_2d, &solution_num, f_sol_data,
+                     R_sol_data, T_sol_data);
   }
 }
 BENCHMARK_TEMPLATE(BM_P35P, double);
@@ -44,10 +43,10 @@ template <class Type> void BM_P4P(benchmark::State &state) {
   Matrix<Type, 3, 1> T_sol_data[10];
 
   generateData(points_3d, points_2d, f_gen, R_gen, C_gen);
-
+  P4PSolver<Type> p4pSolver;
   for (auto _ : state) {
-    p4p_solver(points_3d, points_2d, eps, &solution_num, f_sol_data, R_sol_data,
-               T_sol_data);
+    p4pSolver.solve(points_3d, points_2d, &solution_num, f_sol_data, R_sol_data,
+                    T_sol_data);
   }
 }
 BENCHMARK_TEMPLATE(BM_P4P, double);
