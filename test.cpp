276ed433ded3b632996cfa26073868b959665d7d
Elizaveta Mironovich <elizavetamironvich@yandex.ru>
2019-11-16 17:34:53 +0300

    Change tests so there are 4 not 8

--- test.cpp
+++ test.cpp
@@ -1,36 +1,36 @@
 #include "datagen.h"
 #include "runFunctions.h"
 #include "gtest/gtest.h"
+#include <utility>
 
-template <class T> class PnPTest : public testing::Test {
+template <class SolverClass> class PnPTest : public testing::Test {
 protected:
   PnPTest() {
     it_num = 1e+4; // number of iterations
-    P35PSolver<T> p35pSolver;
-    P4PSolver<T> p4pSolver;
-    p35pRes = runFunction(p35pSolver, it_num);
-    p4pRes = runFunction(p4pSolver, it_num);
+    SolverClass solver;
+    res = runFunction(solver, it_num);
   };
   int it_num;
-  TestResult p35pRes, p4pRes;
+  TestResult res;
 };
 
 #if GTEST_HAS_TYPED_TEST
 
 using testing::Types;
 
-using PrecisionTypes = ::testing::Types<double, float>;
+using PrecisionTypes = ::testing::Types<P35PSolver<double>, P35PSolver<float>,
+                                        P4PSolver<double>, P4PSolver<float>>;
 TYPED_TEST_SUITE(PnPTest, PrecisionTypes);
 
-TYPED_TEST(PnPTest, P35P) {
-  ASSERT_GT((double)this->p35pRes.existSolutions / this->it_num,
+TYPED_TEST(PnPTest, PnP) {
+  ASSERT_GT((double)this->res.existSolutions / this->it_num,
             0.8); // todo: figure out constants
-  ASSERT_GT((double)this->p35pRes.belowThreshold / this->it_num, 0.8);
+  ASSERT_GT((double)this->res.belowThreshold / this->it_num, 0.8);
 }
 
-TYPED_TEST(PnPTest, P4P) {
-  ASSERT_GT((double)this->p4pRes.existSolutions / this->it_num, 0.8);
-  ASSERT_GT((double)this->p4pRes.belowThreshold / this->it_num, 0.77);
-}
+//TYPED_TEST(PnPTest, P4P) {
+//  ASSERT_GT((double)this->p4pRes.existSolutions / this->it_num, 0.8);
+//  ASSERT_GT((double)this->p4pRes.belowThreshold / this->it_num, 0.77);
+//}
 
 #endif // GTEST_HAS_TYPED_TEST
