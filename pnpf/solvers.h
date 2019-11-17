e525d46330db879c5a785492e4df976e7d708d73
Elizaveta Mironovich <elizavetamironvich@yandex.ru>
2019-11-17 19:00:56 +0300

    Add const modifier to input data

--- pnpf/solvers.h
+++ pnpf/solvers.h
@@ -15,8 +15,8 @@ protected:
 
 public:
   Solver() { e = std::numeric_limits<T>::epsilon(); }
-  void solve(Eigen::Matrix<T, 3, 4> &points_3d,
-             Eigen::Matrix<T, 2, 4> &points_2d, int *n, T *fs,
+  void solve(const Eigen::Matrix<T, 3, 4> &points_3d,
+             const Eigen::Matrix<T, 2, 4> &points_2d, int *n, T *fs,
              Eigen::Matrix<T, 3, 3> *Rs, Eigen::Matrix<T, 3, 1> *Cs,
              T diag = 1) {
     static_cast<SolverClass *>(this)->vSolve(points_3d, points_2d, n, fs, Rs,
@@ -27,14 +27,14 @@ public:
 template <class MatlabSolverClass, class T>
 class MatlabSolver : public Solver<MatlabSolver<MatlabSolverClass, T>, T> {
 public:
-  T *X;
+  T const *X;
   T xy[8];
   int sol_num;
   int f_size[2], r_size[3], t_size[2];
   T f_data[10], r_data[90], t_data[30]; // todo: check sizes
 
-  void dataEigenToMatlab(Eigen::Matrix<T, 3, 4> &points_3d,
-                         Eigen::Matrix<T, 2, 4> &points_2d, T diag) {
+  void dataEigenToMatlab(const Eigen::Matrix<T, 3, 4> &points_3d,
+                         const Eigen::Matrix<T, 2, 4> &points_2d, T diag) {
     X = points_3d.data();
     Eigen::Map<Eigen::Matrix<T, 2, 4>>(xy, 2, 4) = points_2d / diag;
   }
@@ -48,8 +48,8 @@ public:
       Rs[i] = Eigen::Map<Eigen::Matrix<T, 3, 3>>(&(r_data[9 * i]));
     }
   }
-  void vSolve(Eigen::Matrix<T, 3, 4> &points_3d,
-              Eigen::Matrix<T, 2, 4> &points_2d, int *n, T *fs,
+  void vSolve(const Eigen::Matrix<T, 3, 4> &points_3d,
+              const Eigen::Matrix<T, 2, 4> &points_2d, int *n, T *fs,
               Eigen::Matrix<T, 3, 3> *Rs, Eigen::Matrix<T, 3, 1> *Cs, T diag) {
     dataEigenToMatlab(points_3d, points_2d, diag);
     static_cast<MatlabSolverClass *>(this)->vMatlabSolve();
